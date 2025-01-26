e8b7de0597680f55f6be792e0b20ed0a73f150d1
Akhaury Nimitt Verma <nimittv6@gmail.com>
2025-01-26 16:48:14 +0530

    Review

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-
+from lxml import etree
 url = "https://www.goal.com/en-in/live-scores"
 
 response = requests.get(url)
 
 soup = BeautifulSoup(response.content, "html.parser")
 
-print(soup.prettify())
+dom = etree.HTML(str(soup))
+
+live_Score = (dom.xpath('//*[class="live-scores_live-scores-wrapper__lGxsb"]/text'))
+print(live_Score)
